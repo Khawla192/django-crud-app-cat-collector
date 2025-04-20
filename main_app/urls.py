26bc2bbd263eebc33801d36918a7940becab431c
Khawla <202005171@stu.uob.edu.bh>
2025-04-20 19:05:38 +0300

    Rendering Data in Templates, add the home and index page and its css

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,6 @@ from . import views # Import views to connect routes to view functions
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('base/', views.about, name='base'),
+    path('cats/', views.cat_index, name='cat-index'),
 
 ]
