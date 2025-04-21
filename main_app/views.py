f1fbcb21105dc4b95d6f9e6fd082cf16dea54271
Khawla <202005171@stu.uob.edu.bh>
2025-04-21 19:01:09 +0300

    add the Toy model, Django Many-to-Many Relationships

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Cat
+from django.views.generic import ListView, DetailView
+from .models import Cat, Toy
 from .forms import FeedingForm
 
 def home(request):
@@ -15,18 +16,24 @@ def cat_index(request):
 
 def cat_detail(request, cat_id):
     cat = Cat.objects.get(id=cat_id)
+    toys_cat_doesnt_have = Toy.objects.exclude(id__in = cat.toys.all().values_list('id'))
     feeding_form = FeedingForm()
     return render(request, 'cats/detail.html', {
-        'cat': cat, 'feeding_form': feeding_form
+        'cat': cat,
+        'feeding_form': feeding_form,
+        'toys': toys_cat_doesnt_have
     })
+
+def associate_toy(request, cat_id, toy_id):
+    Cat.objects.get(id=cat_id).toys.add(toy_id)
+    return redirect('cat-detail', cat_id=cat_id)
 
 class CatCreate(CreateView):
     model = Cat
-    fields = '__all__'
+    fields = ['name', 'breed', 'description', 'age']
 
 class CatUpdate(UpdateView):
     model = Cat
-    # Let's disallow the renaming of a cat by excluding the name field!
     fields = ['breed', 'description', 'age']
 
 class CatDelete(DeleteView):
@@ -34,13 +41,33 @@ class CatDelete(DeleteView):
     success_url = '/cats/'
 
 def add_feeding(request, cat_id):
-    # create a ModelForm instance using the data in request.POST
     form = FeedingForm(request.POST)
-    # validate the form
     if form.is_valid():
-        # don't save the form to the db until it
-        # has the cat_id assigned
         new_feeding = form.save(commit=False)
         new_feeding.cat_id = cat_id
         new_feeding.save()
     return redirect('cat-detail', cat_id=cat_id)
+
+class ToyCreate(CreateView):
+    model = Toy
+    fields = '__all__'
+
+class ToyList(ListView):
+    model = Toy
+
+class ToyDetail(DetailView):
+    model = Toy
+
+class ToyUpdate(UpdateView):
+    model = Toy
+    fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+    model = Toy
+    success_url = '/toys/'
+
+def remove_toy(request, cat_id, toy_id):
+    cat = Cat.objects.get(id=cat_id)
+    toy = Toy.objects.get(id=toy_id)
+    cat.toys.remove(toy)
+    return redirect('cat-detail', cat_id=cat.id)
